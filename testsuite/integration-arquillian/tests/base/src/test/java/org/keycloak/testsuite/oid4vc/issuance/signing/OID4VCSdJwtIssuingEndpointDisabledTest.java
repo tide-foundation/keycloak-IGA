keycloak/keycloak

[OID4VCI] Confine test realm setup to TestCase.configureTestRealm()
Signed-off-by: Thomas Diesler <tdiesler@ibm.com>

testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/oid4vc/issuance/signing/OID4VCSdJwtIssuingEndpointDisabledTest.java
@@ -1,13 +1,12 @@
 package org.keycloak.testsuite.oid4vc.issuance.signing;
 
-import java.util.List;
 import java.util.function.Consumer;
 
 import jakarta.ws.rs.core.Response;
 
-import org.keycloak.common.crypto.CryptoIntegration;
 import org.keycloak.protocol.oid4vc.issuance.OID4VCIssuerEndpoint;
 import org.keycloak.protocol.oid4vc.model.CredentialRequest;
+import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.services.CorsErrorResponseException;
 import org.keycloak.services.managers.AppAuthManager;
@@ -15,12 +14,7 @@ import org.keycloak.testsuite.Assert;
 import org.keycloak.util.JsonSerialization;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.Before;
 import org.junit.Test;
-
-import static org.keycloak.testsuite.forms.PassThroughClientAuthenticator.clientId;
-import static org.keycloak.testsuite.forms.PassThroughClientAuthenticator.namedClientId;
 
 import static org.junit.Assert.assertEquals;
 
@@ -30,37 +24,13 @@ import static org.junit.Assert.assertEquals;
 public class OID4VCSdJwtIssuingEndpointDisabledTest extends OID4VCIssuerEndpointTest {
 
     @Override
-    protected boolean shouldEnableOid4vci() {
+    protected boolean oid4vciRealmEnabled(RealmRepresentation testRealm) {
         return false;
     }
 
     @Override
-    public void configureTestRealm(RealmRepresentation testRealm) {
-        super.configureTestRealm(testRealm);
-        testRealm.setVerifiableCredentialsEnabled(false);
-    }
-
-    /**
-     * Override setup to skip creating oid4vc client scopes when verifiable credentials is disabled.
-     * The parent setup() tries to create client scopes with oid4vc protocol, which will fail
-     * with the new validation that prevents creating oid4vc scopes when VC is disabled.
-     */
-    @Override
-    @Before
-    public void setup() {
-        CryptoIntegration.init(this.getClass().getClassLoader());
-        httpClient = HttpClientBuilder.create().build();
-        client = testRealm().clients().findByClientId(clientId).get(0);
-        namedClient = testRealm().clients().findByClientId(namedClientId).get(0);
-
-        // Skip creating oid4vc client scopes when VC is disabled - they cannot be created
-        // and are not needed for these tests which verify that endpoints reject calls
-
-        List.of(client, namedClient).forEach(client -> {
-            String clientId = client.getClientId();
-            // Enable OID4VCI for the client by default, but allow tests to override
-            setClientOid4vciEnabled(clientId, shouldEnableOid4vci());
-        });
+    protected boolean oid4vciClientEnabled(ClientRepresentation testClient) {
+        return false;
     }
 
     @Test

tide-foundation/keycloak-IGA

testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/oid4vc/issuance/signing/OID4VCSdJwtIssuingEndpointDisabledTest.java
@@ -1,13 +1,12 @@
 package org.keycloak.testsuite.oid4vc.issuance.signing;
 
-import java.util.List;
 import java.util.function.Consumer;
 
 import jakarta.ws.rs.core.Response;
 
-import org.keycloak.common.crypto.CryptoIntegration;
 import org.keycloak.protocol.oid4vc.issuance.OID4VCIssuerEndpoint;
 import org.keycloak.protocol.oid4vc.model.CredentialRequest;
+import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.services.CorsErrorResponseException;
 import org.keycloak.services.managers.AppAuthManager;
@@ -15,12 +14,7 @@ import org.keycloak.testsuite.Assert;
 import org.keycloak.util.JsonSerialization;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.Before;
 import org.junit.Test;
-
-import static org.keycloak.testsuite.forms.PassThroughClientAuthenticator.clientId;
-import static org.keycloak.testsuite.forms.PassThroughClientAuthenticator.namedClientId;
 
 import static org.junit.Assert.assertEquals;
 
@@ -30,37 +24,13 @@ import static org.junit.Assert.assertEquals;
 public class OID4VCSdJwtIssuingEndpointDisabledTest extends OID4VCIssuerEndpointTest {
 
     @Override
-    protected boolean shouldEnableOid4vci() {
+    protected boolean shouldEnableOid4vci(RealmRepresentation testRealm) {
         return false;
     }
 
     @Override
-    public void configureTestRealm(RealmRepresentation testRealm) {
-        super.configureTestRealm(testRealm);
-        testRealm.setVerifiableCredentialsEnabled(false);
-    }
-
-    /**
-     * Override setup to skip creating oid4vc client scopes when verifiable credentials is disabled.
-     * The parent setup() tries to create client scopes with oid4vc protocol, which will fail
-     * with the new validation that prevents creating oid4vc scopes when VC is disabled.
-     */
-    @Override
-    @Before
-    public void setup() {
-        CryptoIntegration.init(this.getClass().getClassLoader());
-        httpClient = HttpClientBuilder.create().build();
-        client = testRealm().clients().findByClientId(clientId).get(0);
-        namedClient = testRealm().clients().findByClientId(namedClientId).get(0);
-
-        // Skip creating oid4vc client scopes when VC is disabled - they cannot be created
-        // and are not needed for these tests which verify that endpoints reject calls
-
-        List.of(client, namedClient).forEach(client -> {
-            String clientId = client.getClientId();
-            // Enable OID4VCI for the client by default, but allow tests to override
-            setClientOid4vciEnabled(clientId, shouldEnableOid4vci());
-        });
+    protected boolean shouldEnableOid4vci(ClientRepresentation testClient) {
+        return false;
     }
 
     @Test