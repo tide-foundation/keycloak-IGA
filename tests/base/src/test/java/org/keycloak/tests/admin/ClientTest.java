keycloak/keycloak

Consider case-sensitivity when validating prohibited schemes of client URIs (#694)
Closes #CVE-2026-9086

Signed-off-by: mposolda <mposolda@gmail.com>

tests/base/src/test/java/org/keycloak/tests/admin/ClientTest.java
@@ -47,6 +47,7 @@ import org.keycloak.crypto.Algorithm;
 import org.keycloak.events.admin.OperationType;
 import org.keycloak.events.admin.ResourceType;
 import org.keycloak.models.AccountRoles;
+import org.keycloak.models.CibaConfig;
 import org.keycloak.models.Constants;
 import org.keycloak.protocol.oidc.OIDCAdvancedConfigWrapper;
 import org.keycloak.protocol.oidc.OIDCConfigAttributes;
@@ -94,6 +95,7 @@ import org.junit.jupiter.api.Test;
 
 import static java.util.Arrays.asList;
 
+import static org.keycloak.models.Constants.OIDC_PROTOCOL;
 import static org.keycloak.models.Constants.defaultClients;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -226,6 +228,83 @@ public class ClientTest {
         }
     }
 
+    @Test
+    public void testCreateClientWithSpecialUrls() {
+        // Client with rootUrl set to AUTH_ADMIN_URL_PROP
+        ClientRepresentation rep = ClientConfigBuilder.create()
+                .clientId("custom1")
+                .description("blank")
+                .enabled(true)
+                .publicClient(true)
+                .protocol(OIDC_PROTOCOL)
+                .rootUrl(Constants.AUTH_ADMIN_URL_PROP)
+                .redirectUris("/foo/*")
+                .build();
+
+        try (Response response = managedRealm.admin().clients().create(rep)) {
+            String id = ApiUtil.getCreatedId(response);
+            managedRealm.cleanup().add(r -> r.clients().get(id).remove());
+            ClientRepresentation found = AdminApiUtil.findClientByClientId(managedRealm.admin(), "custom1").toRepresentation();
+            assertNotNull(found);
+        }
+
+        // Client with rootUrl set to AUTH_ADMIN_URL_PROP
+        rep = ClientConfigBuilder.create()
+                .clientId("custom2")
+                .description("blank")
+                .enabled(true)
+                .publicClient(true)
+                .protocol(OIDC_PROTOCOL)
+                .rootUrl(Constants.AUTH_BASE_URL_PROP)
+                .redirectUris("/foo/*")
+                .build();
+
+        try (Response response = managedRealm.admin().clients().create(rep)) {
+            String id = ApiUtil.getCreatedId(response);
+            managedRealm.cleanup().add(r -> r.clients().get(id).remove());
+            ClientRepresentation found = AdminApiUtil.findClientByClientId(managedRealm.admin(), "custom2").toRepresentation();
+            assertNotNull(found);
+        }
+
+        // Client with rootUrl set to AUTH_ADMIN_URL_PROP
+        rep = ClientConfigBuilder.create()
+                .clientId("custom3")
+                .description("blank")
+                .enabled(true)
+                .publicClient(true)
+                .protocol(OIDC_PROTOCOL)
+                .rootUrl("https://foo")
+                .adminUrl("https://${application.session.host}/bar")
+                .redirectUris("/foo/*")
+                .build();
+
+        try (Response response = managedRealm.admin().clients().create(rep)) {
+            String id = ApiUtil.getCreatedId(response);
+            managedRealm.cleanup().add(r -> r.clients().get(id).remove());
+            ClientRepresentation found = AdminApiUtil.findClientByClientId(managedRealm.admin(), "custom3").toRepresentation();
+            assertNotNull(found);
+        }
+
+        rep = ClientConfigBuilder.create()
+                .clientId("custom4")
+                .description("blank")
+                .enabled(true)
+                .publicClient(true)
+                .protocol(OIDC_PROTOCOL)
+                .rootUrl("https://foo")
+                .adminUrl("https://${application.session.host}/bar")
+                .redirectUris("/foo/*")
+                .webOrigins("127.0.0.1", "localhost", "127.0.0.1:8080", "localhost:8443")
+                .build();
+
+        try (Response response = managedRealm.admin().clients().create(rep)) {
+            String id = ApiUtil.getCreatedId(response);
+            managedRealm.cleanup().add(r -> r.clients().get(id).remove());
+            ClientRepresentation found = AdminApiUtil.findClientByClientId(managedRealm.admin(), "custom4").toRepresentation();
+            assertNotNull(found);
+        }
+    }
+
 
     @Test
     public void testInvalidLengthClientIdValidation() {
@@ -247,31 +326,28 @@ public class ClientTest {
 
     @Test
     public void testInvalidUrlClientValidation() {
-        testClientUriValidation("Root URL is not a valid URL",
-                "Base URL is not a valid URL",
-                "Backchannel logout URL is not a valid URL",
-                null,
+        List<String> urlsToValidate = List.of("Root URL", "Base URL");
+        testClientUriValidation(urlsToValidate, " is not a valid URL",
                 "invalid", "myapp://some-fake-app");
     }
 
     @Test
     public void testIllegalSchemeClientValidation() {
-        testClientUriValidation("Root URL uses an illegal scheme",
-                "Base URL uses an illegal scheme",
-                "Backchannel logout URL uses an illegal scheme",
-                "A redirect URI uses an illegal scheme",
+        List<String> urlsToValidate = List.of("Root URL", "Base URL", "Backchannel logout URL", "A redirect URI", "A post-logout redirect URI",
+                "Admin URL", "JWKS URL", "Logo URL", "Front-channel logout URL", "CIBA notification endpoint", "Valid request URIs");
+        testClientUriValidation(urlsToValidate, " uses an illegal scheme",
                 "data:text/html;base64,PHNjcmlwdD5jb25maXJtKGRvY3VtZW50LmRvbWFpbik7PC9zY3JpcHQ+",
-                "javascript:confirm(document.domain)/*"
+                "DaTa:text/html;base64,PHNjcmlwdD5jb25maXJtKGRvY3VtZW50LmRvbWFpbik7PC9zY3JpcHQ+",
+                "javascript:confirm(document.domain)/*",
+                "jaVaSCript:confirm(document.domain)/*"
         );
     }
 
     // KEYCLOAK-3421
     @Test
     public void testFragmentProhibitedClientValidation() {
-        testClientUriValidation("Root URL must not contain an URL fragment",
-                null,
-                null,
-                "Redirect URIs must not contain an URI fragment",
+        List<String> urlsToValidate = List.of("Root URL", "A redirect URI", "A post-logout redirect URI");
+        testClientUriValidation(urlsToValidate, " must not contain an URL fragment",
                 "http://redhat.com/abcd#someFragment"
         );
     }
@@ -318,12 +394,12 @@ public class ClientTest {
         }
     }
 
-    private void testClientUriValidation(String expectedRootUrlError, String expectedBaseUrlError, String expectedBackchannelLogoutUrlError, String expectedRedirectUrisError, String... testUrls) {
-        testClientUriValidation(false, expectedRootUrlError, expectedBaseUrlError, expectedBackchannelLogoutUrlError, expectedRedirectUrisError, testUrls);
-        testClientUriValidation(true, expectedRootUrlError, expectedBaseUrlError, expectedBackchannelLogoutUrlError, expectedRedirectUrisError, testUrls);
+    private void testClientUriValidation(List<String> urlsToValidate, String errorSuffix, String... testUrls) {
+        testClientUriValidation(false, urlsToValidate, errorSuffix, testUrls);
+        testClientUriValidation(true, urlsToValidate, errorSuffix, testUrls);
     }
 
-    private void testClientUriValidation(boolean create, String expectedRootUrlError, String expectedBaseUrlError, String expectedBackchannelLogoutUrlError, String expectedRedirectUrisError, String... testUrls) {
+    private void testClientUriValidation(boolean create, List<String> urlsToValidate, String errorSuffix, String... testUrls) {
         ClientRepresentation rep;
         if (create) {
             rep = new ClientRepresentation();
@@ -334,29 +410,87 @@ public class ClientTest {
         }
 
         for (String testUrl : testUrls) {
+            String expectedRootUrlError = urlsToValidate.contains("Root URL") ? "Root URL" + errorSuffix : null;
             if (expectedRootUrlError != null) {
                 rep.setRootUrl(testUrl);
                 createOrUpdateClientExpectingValidationErrors(rep, create, expectedRootUrlError);
             }
             rep.setRootUrl(null);
 
+            String expectedBaseUrlError = urlsToValidate.contains("Base URL") ? "Base URL" + errorSuffix : null;
             if (expectedBaseUrlError != null) {
                 rep.setBaseUrl(testUrl);
                 createOrUpdateClientExpectingValidationErrors(rep, create, expectedBaseUrlError);
             }
             rep.setBaseUrl(null);
 
+            String expectedBackchannelLogoutUrlError = urlsToValidate.contains("Backchannel logout URL") ? "Backchannel logout URL" + errorSuffix : null;
             if (expectedBackchannelLogoutUrlError != null) {
                 OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setBackchannelLogoutUrl(testUrl);
                 createOrUpdateClientExpectingValidationErrors(rep, create, expectedBackchannelLogoutUrlError);
             }
             OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setBackchannelLogoutUrl(null);
 
+            String expectedRedirectUrisError = urlsToValidate.contains("A redirect URI") ? "A redirect URI" + errorSuffix : null;
             if (expectedRedirectUrisError != null) {
                 rep.setRedirectUris(List.of(testUrl));
                 createOrUpdateClientExpectingValidationErrors(rep, create, expectedRedirectUrisError);
             }
             rep.setRedirectUris(null);
+
+            String expectedPostLogoutRedirectUrisError = urlsToValidate.contains("A post-logout redirect URI") ? "A post-logout redirect URI" + errorSuffix : null;
+            if (expectedPostLogoutRedirectUrisError != null) {
+                OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setPostLogoutRedirectUris(List.of(testUrl));
+                createOrUpdateClientExpectingValidationErrors(rep, create, expectedPostLogoutRedirectUrisError);
+            }
+            OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setPostLogoutRedirectUris(null);
+
+            String expectedAdminUrlError = urlsToValidate.contains("Admin URL") ? "Admin URL" + errorSuffix : null;
+            if (expectedAdminUrlError != null) {
+                rep.setAdminUrl(testUrl);
+                createOrUpdateClientExpectingValidationErrors(rep, create, expectedAdminUrlError);
+            }
+            rep.setAdminUrl(null);
+
+            String expectedJwksUrlError = urlsToValidate.contains("JWKS URL") ? "JWKS URL" + errorSuffix : null;
+            if (expectedJwksUrlError != null) {
+                OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setJwksUrl(testUrl);
+                createOrUpdateClientExpectingValidationErrors(rep, create, expectedJwksUrlError);
+            }
+            OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setJwksUrl(null);
+
+            String expectedFrontChannelLogoutUrl = urlsToValidate.contains("Front-channel logout URL") ? "Front-channel logout URL" + errorSuffix : null;
+            if (expectedFrontChannelLogoutUrl != null) {
+                OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setFrontChannelLogoutUrl(testUrl);
+                createOrUpdateClientExpectingValidationErrors(rep, create, expectedFrontChannelLogoutUrl);
+            }
+            OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setFrontChannelLogoutUrl(null);
+
+            String expectedCibaNotificationEndpointError = urlsToValidate.contains("CIBA notification endpoint") ? "The url [backchannel_client_notification_endpoint] is malformed" : null;
+            if (expectedCibaNotificationEndpointError != null) {
+                rep.getAttributes().put(CibaConfig.CIBA_BACKCHANNEL_CLIENT_NOTIFICATION_ENDPOINT, testUrl);
+                createOrUpdateClientExpectingValidationErrors(rep, create, expectedCibaNotificationEndpointError);
+            }
+            if (rep.getAttributes() != null) {
+                rep.getAttributes().remove(CibaConfig.CIBA_BACKCHANNEL_CLIENT_NOTIFICATION_ENDPOINT);
+            }
+
+            // Test Logo URL only for "javascript" scheme
+            if (testUrl.toLowerCase().startsWith("javascript")) {
+                String logoURLError = urlsToValidate.contains("Logo URL") ? "Logo URL" + errorSuffix : null;
+                if (logoURLError != null) {
+                    OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setLogoUri(testUrl);
+                    createOrUpdateClientExpectingValidationErrors(rep, create, logoURLError);
+                }
+                OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setLogoUri(null);
+            }
+
+            String expectedValidRequestURIsError = urlsToValidate.contains("Valid request URIs") ? "Valid request URIs has a value with illegal scheme" : null;
+            if (expectedValidRequestURIsError != null) {
+                OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setRequestUris(List.of(testUrl));
+                createOrUpdateClientExpectingValidationErrors(rep, create, expectedValidRequestURIsError);
+            }
+            OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setRequestUris(null);
 
             if (expectedRootUrlError != null) {
                 rep.setRootUrl(testUrl);
@@ -382,7 +516,7 @@ public class ClientTest {
         } else {
             try {
                 managedRealm.admin().clients().get(rep.getId()).update(rep);
-                fail("Expected exception");
+                fail("Expected exception: " + List.of(expectedErrors));
             } catch (BadRequestException e) {
                 response = e.getResponse();
             }

tide-foundation/keycloak-IGA

closes #50347
(cherry picked from commit 32cdceb80aeb43dc082bb032a87daa5105ea1fbd)

tests/base/src/test/java/org/keycloak/tests/admin/ClientTest.java
@@ -50,6 +50,7 @@ import org.keycloak.crypto.Algorithm;
 import org.keycloak.events.admin.OperationType;
 import org.keycloak.events.admin.ResourceType;
 import org.keycloak.models.AccountRoles;
+import org.keycloak.models.CibaConfig;
 import org.keycloak.models.Constants;
 import org.keycloak.models.utils.SystemClientUtil;
 import org.keycloak.protocol.oidc.OIDCAdvancedConfigWrapper;
@@ -99,6 +100,7 @@ import org.junit.jupiter.api.Test;
 
 import static java.util.Arrays.asList;
 
+import static org.keycloak.models.Constants.OIDC_PROTOCOL;
 import static org.keycloak.models.Constants.REALM_MANAGEMENT_CLIENT_ID;
 import static org.keycloak.models.Constants.defaultClients;
 
@@ -252,6 +254,83 @@ public class ClientTest {
         }
     }
 
+    @Test
+    public void testCreateClientWithSpecialUrls() {
+        // Client with rootUrl set to AUTH_ADMIN_URL_PROP
+        ClientRepresentation rep = ClientBuilder.create()
+                .clientId("custom1")
+                .description("blank")
+                .enabled(true)
+                .publicClient(true)
+                .protocol(OIDC_PROTOCOL)
+                .rootUrl(Constants.AUTH_ADMIN_URL_PROP)
+                .redirectUris("/foo/*")
+                .build();
+
+        try (Response response = managedRealm.admin().clients().create(rep)) {
+            String id = ApiUtil.getCreatedId(response);
+            managedRealm.cleanup().add(r -> r.clients().get(id).remove());
+            ClientRepresentation found = AdminApiUtil.findClientByClientId(managedRealm.admin(), "custom1").toRepresentation();
+            assertNotNull(found);
+        }
+
+        // Client with rootUrl set to AUTH_ADMIN_URL_PROP
+        rep = ClientBuilder.create()
+                .clientId("custom2")
+                .description("blank")
+                .enabled(true)
+                .publicClient(true)
+                .protocol(OIDC_PROTOCOL)
+                .rootUrl(Constants.AUTH_BASE_URL_PROP)
+                .redirectUris("/foo/*")
+                .build();
+
+        try (Response response = managedRealm.admin().clients().create(rep)) {
+            String id = ApiUtil.getCreatedId(response);
+            managedRealm.cleanup().add(r -> r.clients().get(id).remove());
+            ClientRepresentation found = AdminApiUtil.findClientByClientId(managedRealm.admin(), "custom2").toRepresentation();
+            assertNotNull(found);
+        }
+
+        // Client with rootUrl set to AUTH_ADMIN_URL_PROP
+        rep = ClientBuilder.create()
+                .clientId("custom3")
+                .description("blank")
+                .enabled(true)
+                .publicClient(true)
+                .protocol(OIDC_PROTOCOL)
+                .rootUrl("https://foo")
+                .adminUrl("https://${application.session.host}/bar")
+                .redirectUris("/foo/*")
+                .build();
+
+        try (Response response = managedRealm.admin().clients().create(rep)) {
+            String id = ApiUtil.getCreatedId(response);
+            managedRealm.cleanup().add(r -> r.clients().get(id).remove());
+            ClientRepresentation found = AdminApiUtil.findClientByClientId(managedRealm.admin(), "custom3").toRepresentation();
+            assertNotNull(found);
+        }
+
+        rep = ClientBuilder.create()
+                .clientId("custom4")
+                .description("blank")
+                .enabled(true)
+                .publicClient(true)
+                .protocol(OIDC_PROTOCOL)
+                .rootUrl("https://foo")
+                .adminUrl("https://${application.session.host}/bar")
+                .redirectUris("/foo/*")
+                .webOrigins("127.0.0.1", "localhost", "127.0.0.1:8080", "localhost:8443")
+                .build();
+
+        try (Response response = managedRealm.admin().clients().create(rep)) {
+            String id = ApiUtil.getCreatedId(response);
+            managedRealm.cleanup().add(r -> r.clients().get(id).remove());
+            ClientRepresentation found = AdminApiUtil.findClientByClientId(managedRealm.admin(), "custom4").toRepresentation();
+            assertNotNull(found);
+        }
+    }
+
 
     @Test
     public void testInvalidLengthClientIdValidation() {
@@ -273,31 +352,28 @@ public class ClientTest {
 
     @Test
     public void testInvalidUrlClientValidation() {
-        testClientUriValidation("Root URL is not a valid URL",
-                "Base URL is not a valid URL",
-                "Backchannel logout URL is not a valid URL",
-                null,
+        List<String> urlsToValidate = List.of("Root URL", "Base URL");
+        testClientUriValidation(urlsToValidate, " is not a valid URL",
                 "invalid", "myapp://some-fake-app");
     }
 
     @Test
     public void testIllegalSchemeClientValidation() {
-        testClientUriValidation("Root URL uses an illegal scheme",
-                "Base URL uses an illegal scheme",
-                "Backchannel logout URL uses an illegal scheme",
-                "A redirect URI uses an illegal scheme",
+        List<String> urlsToValidate = List.of("Root URL", "Base URL", "Backchannel logout URL", "A redirect URI", "A post-logout redirect URI",
+                "Admin URL", "JWKS URL", "Logo URL", "Front-channel logout URL", "CIBA notification endpoint", "Valid request URIs");
+        testClientUriValidation(urlsToValidate, " uses an illegal scheme",
                 "data:text/html;base64,PHNjcmlwdD5jb25maXJtKGRvY3VtZW50LmRvbWFpbik7PC9zY3JpcHQ+",
-                "javascript:confirm(document.domain)/*"
+                "DaTa:text/html;base64,PHNjcmlwdD5jb25maXJtKGRvY3VtZW50LmRvbWFpbik7PC9zY3JpcHQ+",
+                "javascript:confirm(document.domain)/*",
+                "jaVaSCript:confirm(document.domain)/*"
         );
     }
 
     // KEYCLOAK-3421
     @Test
     public void testFragmentProhibitedClientValidation() {
-        testClientUriValidation("Root URL must not contain an URL fragment",
-                null,
-                null,
-                "Redirect URIs must not contain an URI fragment",
+        List<String> urlsToValidate = List.of("Root URL", "A redirect URI", "A post-logout redirect URI");
+        testClientUriValidation(urlsToValidate, " must not contain an URL fragment",
                 "http://redhat.com/abcd#someFragment"
         );
     }
@@ -344,12 +420,12 @@ public class ClientTest {
         }
     }
 
-    private void testClientUriValidation(String expectedRootUrlError, String expectedBaseUrlError, String expectedBackchannelLogoutUrlError, String expectedRedirectUrisError, String... testUrls) {
-        testClientUriValidation(false, expectedRootUrlError, expectedBaseUrlError, expectedBackchannelLogoutUrlError, expectedRedirectUrisError, testUrls);
-        testClientUriValidation(true, expectedRootUrlError, expectedBaseUrlError, expectedBackchannelLogoutUrlError, expectedRedirectUrisError, testUrls);
+    private void testClientUriValidation(List<String> urlsToValidate, String errorSuffix, String... testUrls) {
+        testClientUriValidation(false, urlsToValidate, errorSuffix, testUrls);
+        testClientUriValidation(true, urlsToValidate, errorSuffix, testUrls);
     }
 
-    private void testClientUriValidation(boolean create, String expectedRootUrlError, String expectedBaseUrlError, String expectedBackchannelLogoutUrlError, String expectedRedirectUrisError, String... testUrls) {
+    private void testClientUriValidation(boolean create, List<String> urlsToValidate, String errorSuffix, String... testUrls) {
         ClientRepresentation rep;
         if (create) {
             rep = new ClientRepresentation();
@@ -360,29 +436,87 @@ public class ClientTest {
         }
 
         for (String testUrl : testUrls) {
+            String expectedRootUrlError = urlsToValidate.contains("Root URL") ? "Root URL" + errorSuffix : null;
             if (expectedRootUrlError != null) {
                 rep.setRootUrl(testUrl);
                 createOrUpdateClientExpectingValidationErrors(rep, create, expectedRootUrlError);
             }
             rep.setRootUrl(null);
 
+            String expectedBaseUrlError = urlsToValidate.contains("Base URL") ? "Base URL" + errorSuffix : null;
             if (expectedBaseUrlError != null) {
                 rep.setBaseUrl(testUrl);
                 createOrUpdateClientExpectingValidationErrors(rep, create, expectedBaseUrlError);
             }
             rep.setBaseUrl(null);
 
+            String expectedBackchannelLogoutUrlError = urlsToValidate.contains("Backchannel logout URL") ? "Backchannel logout URL" + errorSuffix : null;
             if (expectedBackchannelLogoutUrlError != null) {
                 OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setBackchannelLogoutUrl(testUrl);
                 createOrUpdateClientExpectingValidationErrors(rep, create, expectedBackchannelLogoutUrlError);
             }
             OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setBackchannelLogoutUrl(null);
 
+            String expectedRedirectUrisError = urlsToValidate.contains("A redirect URI") ? "A redirect URI" + errorSuffix : null;
             if (expectedRedirectUrisError != null) {
                 rep.setRedirectUris(List.of(testUrl));
                 createOrUpdateClientExpectingValidationErrors(rep, create, expectedRedirectUrisError);
             }
             rep.setRedirectUris(null);
+
+            String expectedPostLogoutRedirectUrisError = urlsToValidate.contains("A post-logout redirect URI") ? "A post-logout redirect URI" + errorSuffix : null;
+            if (expectedPostLogoutRedirectUrisError != null) {
+                OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setPostLogoutRedirectUris(List.of(testUrl));
+                createOrUpdateClientExpectingValidationErrors(rep, create, expectedPostLogoutRedirectUrisError);
+            }
+            OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setPostLogoutRedirectUris(null);
+
+            String expectedAdminUrlError = urlsToValidate.contains("Admin URL") ? "Admin URL" + errorSuffix : null;
+            if (expectedAdminUrlError != null) {
+                rep.setAdminUrl(testUrl);
+                createOrUpdateClientExpectingValidationErrors(rep, create, expectedAdminUrlError);
+            }
+            rep.setAdminUrl(null);
+
+            String expectedJwksUrlError = urlsToValidate.contains("JWKS URL") ? "JWKS URL" + errorSuffix : null;
+            if (expectedJwksUrlError != null) {
+                OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setJwksUrl(testUrl);
+                createOrUpdateClientExpectingValidationErrors(rep, create, expectedJwksUrlError);
+            }
+            OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setJwksUrl(null);
+
+            String expectedFrontChannelLogoutUrl = urlsToValidate.contains("Front-channel logout URL") ? "Front-channel logout URL" + errorSuffix : null;
+            if (expectedFrontChannelLogoutUrl != null) {
+                OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setFrontChannelLogoutUrl(testUrl);
+                createOrUpdateClientExpectingValidationErrors(rep, create, expectedFrontChannelLogoutUrl);
+            }
+            OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setFrontChannelLogoutUrl(null);
+
+            String expectedCibaNotificationEndpointError = urlsToValidate.contains("CIBA notification endpoint") ? "The url [backchannel_client_notification_endpoint] is malformed" : null;
+            if (expectedCibaNotificationEndpointError != null) {
+                rep.getAttributes().put(CibaConfig.CIBA_BACKCHANNEL_CLIENT_NOTIFICATION_ENDPOINT, testUrl);
+                createOrUpdateClientExpectingValidationErrors(rep, create, expectedCibaNotificationEndpointError);
+            }
+            if (rep.getAttributes() != null) {
+                rep.getAttributes().remove(CibaConfig.CIBA_BACKCHANNEL_CLIENT_NOTIFICATION_ENDPOINT);
+            }
+
+            // Test Logo URL only for "javascript" scheme
+            if (testUrl.toLowerCase().startsWith("javascript")) {
+                String logoURLError = urlsToValidate.contains("Logo URL") ? "Logo URL" + errorSuffix : null;
+                if (logoURLError != null) {
+                    OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setLogoUri(testUrl);
+                    createOrUpdateClientExpectingValidationErrors(rep, create, logoURLError);
+                }
+                OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setLogoUri(null);
+            }
+
+            String expectedValidRequestURIsError = urlsToValidate.contains("Valid request URIs") ? "Valid request URIs has a value with illegal scheme" : null;
+            if (expectedValidRequestURIsError != null) {
+                OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setRequestUris(List.of(testUrl));
+                createOrUpdateClientExpectingValidationErrors(rep, create, expectedValidRequestURIsError);
+            }
+            OIDCAdvancedConfigWrapper.fromClientRepresentation(rep).setRequestUris(null);
 
             if (expectedRootUrlError != null) {
                 rep.setRootUrl(testUrl);
@@ -408,7 +542,7 @@ public class ClientTest {
         } else {
             try {
                 managedRealm.admin().clients().get(rep.getId()).update(rep);
-                fail("Expected exception");
+                fail("Expected exception: " + List.of(expectedErrors));
             } catch (BadRequestException e) {
                 response = e.getResponse();
             }